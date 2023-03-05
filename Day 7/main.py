f50b24cdd65502cff5653c38f62e7a14c10905a1
simonedaini <simonedaini1993@gmail.com>
2023-03-05 10:15:37 +0100

    05/03

--- Day 7/main.py
+++ Day 7/main.py
@@ -6,10 +6,7 @@ d = {}
 count = 0
 current_path = ""
 for line in lines:
-    if count == 10000000000000000:
-        break
-    else:
-        count += 1
+    print("line = {}".format(line))
     if " cd " in line:
         if ".." in line:
             if len(current_path.split("/")) > 2:
@@ -24,23 +21,26 @@ for line in lines:
                     current_path = current_path + line.replace("$ cd ", "").strip()
                 else:
                     current_path = current_path + "/" + line.replace("$ cd ", "").strip()
-        print(current_path)
+        print("current_path = {}".format(current_path) )
         if current_path not in d:
             d[current_path] = 0
     elif "dir" in line or "ls" in line:
         pass
     else:
         size = int(line.split(" ")[0])
-        path = current_path.split("/")
-        for i in range(len(path)-1):
-            if i == 0:
-                current = "/".join(path[:])
-            else:
-                current = "/".join(path[:-i])
-            print("{}) Adding to {}".format(i, current))
-            d[current] += size
-        d["/"] += size
-
+        if current_path == "/":
+            print("Adding {} to /".format(size))
+            d["/"] += size
+        else:
+            path = current_path.split("/")
+            for i in range(len(path)-1):
+                if i == 0:
+                    current = "/".join(path[:])
+                else:
+                    current = "/".join(path[:-i])
+                print("{}) Adding {} to {}".format(i, size, current))
+                d[current] += size
+        
 
 sum = 0
 for path in d:
---
+++ prova.py
@@ -0,0 +1,27 @@
+import os
+ 
+# Clearing the Screen
+os.system('cls')
+
+def shift(s):
+    for i in range(shift):
+        print(" ", end="")
+
+def arm(l):
+    print("/", end="")
+    for i in range(l):
+        print("=", end="")
+    print("\====0")
+
+arm(2)
+def print_gru(right, height, arm):
+    shift(right)
+    # for i in range(arm):
+
+        
+
+
+
+
+
+
